1df210cbb7537be06eaa6b55471dd596c0bb3c59
jakehaggard26 <jhaggard26@gmail.com>
2026-08-31 18:52:14 -0400

    Add to Node Example

--- DSA/Examples/Node_Examples.py
+++ DSA/Examples/Node_Examples.py
@@ -1,5 +1,26 @@
 from DSA.Node import Node
 
-node = Node(42)
-print(node.get_data())  # Output: 42
-print(type(node.get_data()))  # Output: <class 'int'>   
+# Example usage of the Node class using integers
+node1 = Node(42)
+print(node1.get_data())  # Output: 42
+print(type(node1.get_data()))  # Output: <class 'int'>   
+
+node2 = Node(4)
+
+print(f"Node 1 ({node1.get_data()}) is less than Node 2 ({node2.get_data()}): {node1 < node2}")  # Output: False
+print(f"Node 1 ({node1.get_data()}) is greater than Node 2 ({node2.get_data()}): {node1 > node2}")  # Output: True
+
+print(node1.__repr__())  # Output: Node(42)
+
+
+# Example usage of the Node class using strings
+node1 = Node("abc")
+print(node1.get_data())  # Output: abc
+print(type(node1.get_data()))  # Output: <class 'str'>   
+
+node2 = Node("xyz")
+
+print(f"Node 1 ({node1.get_data()}) is less than Node 2 ({node2.get_data()}): {node1 < node2}")  # Output: True
+print(f"Node 1 ({node1.get_data()}) is greater than Node 2 ({node2.get_data()}): {node1 > node2}")  # Output: False
+
+print(node1.__repr__())  # Output: Node(abc)
